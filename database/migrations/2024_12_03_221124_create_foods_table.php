b73e42806c4a7a3b385c8a36da573fb8cc5597c6
Afasarya <coderea9@gmail.com>
2024-12-04 08:35:15 +0700

    first commit

---
+++ database/migrations/2024_12_03_221124_create_foods_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id('id_makanan'); // Primary key menjadi 'id_makanan'
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('foods');
+    }
+};
